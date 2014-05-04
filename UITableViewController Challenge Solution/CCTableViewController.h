bf70604265fe2db20360d3fc00610af5eeb0d5f7
Brandon Riley <Ronin@192.168.1.8>
2014-05-04 10:44:07 -0700

    solved the challenge

---
+++ UITableViewController Challenge Solution/CCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Brandon Riley on 5/4/14.
+//  Copyright (c) 2014 Brandon Riley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTableViewController : UITableViewController
+
+@end
